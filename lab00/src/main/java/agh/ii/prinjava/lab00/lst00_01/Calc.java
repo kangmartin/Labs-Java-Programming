2fbb6906c9dcff8ef04f523ee339c73e61ed8717
Martin Kang <martin.kang@efrei.net>
2023-09-19 15:58:21 +0200

    Changes

--- lab00/src/main/java/agh/ii/prinjava/lab00/lst00_01/Calc.java
+++ lab00/src/main/java/agh/ii/prinjava/lab00/lst00_01/Calc.java
@@ -1,7 +1,15 @@
 package agh.ii.prinjava.lab00.lst00_01;
 
 public class Calc {
+    public double x;
+
+    public Calc(double x) {
+        this.x = x;
+    }
+
     public static int add(int a, int b){
         return a + b;
     }
+
+
 }
