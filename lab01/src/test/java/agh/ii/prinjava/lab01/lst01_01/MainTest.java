77abfb0a7900d3cb5729a810421e97fceccffd0d
Martin Kang <martin.kang@efrei.net>
2023-09-19 16:27:00 +0200

    Tests add

--- lab01/src/test/java/agh/ii/prinjava/lab01/lst01_01/MainTest.java
+++ lab01/src/test/java/agh/ii/prinjava/lab01/lst01_01/MainTest.java
@@ -1,6 +1,5 @@
-package agh.ii.prinjava.lab01.lst01_02;
+package agh.ii.prinjava.lab01.lst01_01;
 
-import agh.ii.prinjava.lab01.lst01_01.HelloEncapsulation;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-
     @Test
     void testIsEncapsulated() {
         HelloEncapsulation enDemo = new HelloEncapsulation(42);
