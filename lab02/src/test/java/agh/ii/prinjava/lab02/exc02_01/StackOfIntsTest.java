4afa1e36cd68973c59fba7d5355d2dd442c53336
Martin Kang <martin.kang@efrei.net>
2023-11-09 17:42:58 +0100

    Update project

--- lab02/src/test/java/agh/ii/prinjava/lab02/exc02_01/StackOfIntsTest.java
+++ lab02/src/test/java/agh/ii/prinjava/lab02/exc02_01/StackOfIntsTest.java
@@ -10,33 +10,26 @@ class StackOfIntsTest {
     @Test
     public void testPushAndPop() {
         StackOfInts stack = new LinkedListBasedImpl();
-
-        // Push some elements onto the stack.
         stack.push(1);
         stack.push(2);
         stack.push(3);
 
-        // Pop the elements off the stack and verify that they are in the reverse order of which they were pushed.
         assertEquals(3, stack.pop());
         assertEquals(2, stack.pop());
         assertEquals(1, stack.pop());
 
-        // Verify that the stack is now empty.
         assertTrue(stack.isEmpty());
     }
 
     @Test
     public void testPopOnEmptyStack() {
         StackOfInts stack = new LinkedListBasedImpl();
-        // Try to pop an element from an empty stack.
         assertThrows(IllegalStateException.class, stack::pop);
     }
 
     @Test
     public void testPeekOnEmptyStack() {
         StackOfInts stack = new LinkedListBasedImpl();
-
-        // Try to peek at the top element of an empty stack.
         assertThrows(IllegalStateException.class, stack::peek);
     }
 
